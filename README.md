7448083ac106455316a799c5424127373be50618
matiisi <matias.kantele@gmail.com>
2013-10-16 13:15:56 +0300

    Login screen layout, refactoring and updated readme with game description

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ multimedia-programming-project
 ==============================
 
 Multimedia programming project featuring WebGL with WebRTC and three.js
+
+
+Project Duellum Solari - Planetary combat game
+----------------------------------------------
+
+Description: 2 player game where each player control a rivaling planet in the same solar system.
+Objective: Destroy your enemy before you are destroyed.
+
+"Battleship with planets"
+
+Gameplay:
+- Units can be used to move or fire on the enemy planet.
+- Each action performed by units result in a cooldown in which the unit is unable to perform actions.
+
+
--- js/main.js
+++ js/main.js
@@ -1,167 +1,104 @@
-//Initialize vars here to make them global
-var peer = undefined;
-var connection = undefined; //Connection to another peer, should be made an array in future for possible multiple connections
+var connectionBroker = undefined;
+var connection = undefined; //Connection to opponent
 var serverInfo = {hostname: '82.130.14.29', port: 7500};
+var playerName = "";
 
-function HandleReceivedData(conn, data){
-	//Game related data receiving goes here
-	$("#dataChannelRecieve").val(data);
-	console.log(data);
+function RegisterToServer() {
+
+	// Register to server using info above. Timeouts/errors if server is not running.
+	// Server is off by default, ask Kura2 in IRC if you need it
+	connectionBroker = new Peer({host: serverInfo.hostname, port: serverInfo.port}, {'iceServers':[{'url':'stun:stun.l.google.com:19302'}]});
+
+	connectionBroker.on('error', function(error){
+		$('#myId').css('color', '#C44D58');
+		$('#myId').css('text-shadow', '0 0 0.1em #C44D58, 0 0 0.1em #C44D58, 0 0 0.1em #C44D58');
+		$('#myId').text("No connection to server");
+	});
+
+	// This is run after we've successfully connected to server and recieved an ID
+	connectionBroker.on('open', function(id) {
+		$('#myId').css('color', '#C7F464');
+		$('#myId').css('text-shadow', '0 0 0.1em #C7F464, 0 0 0.1em #C7F464, 0 0 0.1em #C7F464');
+		$('#myId').text("ID: " + id);
+		console.log("Connected to brokering server");
+	});
+
+	// Set up connection if someone connects to us
+	connectionBroker.on('connection', function(conn) {
+		SetConnectionEvents(conn);
+	});
 }
 
-function SendData(conn, data){
-	//Game mechanics should use this function for sending data
-	conn.send(data);
+// Creates a connection to a given opponents ID
+function ConnectToOpponent(opponentId) {
+
+	// Error checking
+	if(connectionBroker === undefined) {
+		// Shouldn't be needed to test as this should be done on page load
+		alert("Not connected to server");
+		return false;
+	}
+
+	// Get opponents ID to connect to from input field if there's text
+	if(opponentId === undefined) {
+		if($("#connectTo").val() === ""){
+			alert("No opponent ID specified");
+			return false;
+		}
+		opponentId = $("#connectTo").val();
+	}
+
+	// Actual connection
+	var conn = connectionBroker.connect(opponentId);
+
+	// Set up connection when we connect to someone
+	SetConnectionEvents(conn);
 }
 
-function SetConnectionEvents(conn){
+function SetConnectionEvents(conn) {
 	
-	//When connection is established
-	conn.on('open', function(){
+	// When connection is established
+	conn.on('open', function() {
 		connection = conn;
-		$("#ConnectToPeer").text("Connect to Peer");
-		alert('Connection established!');
+		$('#welcomeScreen').css('display', 'none');
+		// Exchange player names
+		playerName = $("#playerName").val();
+		SendData(connection, "Opponent for this match: " + playerName);
 	});
 
-	//When remote peer connection is closed
-	conn.on('close', function(){
+	// When remote peer connection is closed
+	conn.on('close', function() {
 		connection = undefined;
-		alert('Disconnected from peer!');
+		alert('Disconnected from opponent!');
 	});
 
-	//When we recieve data from remote peer
-  	conn.on('data', function(data){
-   		HandleReceivedData(conn, data);
+	// When we recieve data from remote peer
+  	conn.on('data', function(data) {
+   		console.log(data);
 	});
 
-	conn.on('error', function(err){
-		$("#ConnectToPeer").text("Connect to Peer");
+	conn.on('error', function(err) {
 		console.log("ERROR!!!");
 		console.log(err.message);
 	});
 }
 
-function RegisterToServer(){
-
-	//Register to server using info above. Timeouts/errors if server is not running.
-	//Server is off by default, ask Kura2 in IRC if you need it
-	peer = new Peer({host: serverInfo.hostname, port: serverInfo.port}, {'iceServers':[{'url':'stun:stun.l.google.com:19302'}]});
-
-	//Error handling
-	peer.on('error', function(error){
-		alert(error.message);
-	});
-
-	//This is run after we've successfully connected to server and recieved an ID
-	peer.on('open', function(id) {
-		$("#OwnID").text("Own ID: "+id);
-		console.log("Connected to brokering server");
-	});
-
-	//Set up connection if someone connects to us
-	peer.on('connection', function(conn) {
-		SetConnectionEvents(conn);
-	});
+// Game mechanics should use this function for sending data
+function SendData(conn, data) {
+	conn.send(data);
 }
 
-//Creates a connection to a given peer id
-function ConnectToPeer(peerId){
-
-	//Error checking
-	if(peer === undefined){
-		//Shouldn't be needed to test as this should be done on page load
-		alert("Not connected to server");
-		return false;
-	}
-
-	//Get peer ID to connect to from input box text if there's text
-	if(peerId === undefined){
-		if($("#dataChannelSend").val() === ""){
-			alert("No peer ID specified");
-			return false;
-		}
-		peerId = $("#dataChannelSend").val();
-		$("#dataChannelSend").val(""); //Empty after taking the ID
-	}
-
-	//Actual connection
-	var conn = peer.connect(peerId);
-
-	$("#ConnectToPeer").text("Connecting...");
-
-	//Set up connection when we connect to someone
-	SetConnectionEvents(conn);
-}
-
-//This function is run after whole DOM has been loaded
+// This function is run after whole DOM has been loaded
 $(document).ready(function() {
 
-	//Bind functions to html elements
+	// Bind functions to html elements
 	RegisterToServer();
 
-	$("#ConnectToPeer").on('click', function(){
-		ConnectToPeer();
-	});
-
-	$("#Send").on('click', function(){
-		//Get data from textbox (later on game functionality should use the SendData function)
-		var data = $("#dataChannelSend").val();
-		
-		if(connection === undefined){
-			alert("not connected to a peer");
+	$('#connect').on('click', function() {
+		ConnectToOpponent();
+		// Check that the connection was made
+		if(connection === undefined) {
+			alert("Connection to opponent failed");
 		}
-		else if(data === undefined || data === ""){
-			alert("nothing to send!");
-		}
-
-		//Actual send data
-		var k = $("#dataChannelSend").val();
-		SendData(connection, k);
-		$("#dataChannelSend").val(""); //clear textbox
 	});
 });
-
-/*
-
-THREE JS STUFF BELOW
-
-*/
-
-
-/*
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
-var renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-var geometry = new THREE.CubeGeometry(2,2,2);
-var material = new THREE.MeshBasicMaterial({color: 'red', wireframe: true});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-window.addEventListener( 'resize', onWindowResize, false );
-
-var render = function () {
-	requestAnimationFrame(render);
-
-	cube.rotation.x += 0.02;
-	cube.rotation.y += 0.02;
-
-	renderer.render(scene, camera);
-};
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-var peer = new Peer({key: 'jlr92mqbkkai3sor'});
-
-
-render();
-*/
